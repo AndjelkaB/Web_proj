6061a6dcdfb1947b3c17d9c7a353d7852c453906
AndjelkaB <bulat_ana@yahoo.com>
2022-07-02 02:08:36 +0200

    test

--- src/main/resources/templates/data.sql
+++ src/main/resources/templates/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO KORISNIK (id,  koisnickoIme, lozinka, ime, prezime, uloga, datumRodjenja, pol) VALUES (0, 'admin', 'admin', 'Amy', 'Smith', 'ADMIN', '12/20/1996', 'Female');
+
+
+
+INSERT INTO KORISNIK (id,  koisnickoIme, lozinka, ime, prezime, uloga, datumRodjenja, pol) VALUES (0, 'admin', 'admin', 'Amy', 'Smith', 'ADMIN', '12/20/1996', 'Female', true);
