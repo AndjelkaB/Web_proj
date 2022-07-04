cfdc9386f1984dbb4eb1458942c24aa5e33cd186
AndjelkaB <bulat_ana@yahoo.com>
2022-07-04 16:06:23 +0200

    jhkjhk

--- src/main/java/veb/proj/veb/configuration/dataConfig.java
+++ src/main/java/veb/proj/veb/configuration/dataConfig.java
@@ -1,6 +1,11 @@
 package veb.proj.veb.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import veb.proj.veb.entity.Lokacija;
+import veb.proj.veb.entity.Menadzer;
+import veb.proj.veb.entity.Restoran;
+import veb.proj.veb.entity.Uloga;
 import veb.proj.veb.repository.*;
 
 public class dataConfig {
@@ -28,5 +33,17 @@ public class dataConfig {
 
 @Autowired
     private TipKupcaRepository tipKupcaRepository;
+
+    @Bean
+    public boolean Instantiate() {
+
+
+        Menadzer m = new Menadzer();
+        menadzerRepository.save(m);
+        Menadzer mm = new Menadzer();
+        menadzerRepository.save(mm);
+
+        return true;
+    }
 
 }
--- src/main/resources/templates/data.sql
+++ src/main/resources/templates/data.sql
@@ -1,21 +1,23 @@
 
 
 
-INSERT INTO KORISNIK (id,  koisnickoIme, lozinka, ime, prezime, uloga, datumRodjenja, pol) VALUES
+INSERT INTO KORISNIK (id, datumRodjenja, ime, koisnickoIme, lozinka, ime, pol, prezime, uloga  ) VALUES
 (0, 'admin', 'admin', 'Amy', 'Smith', 'ADMIN', '12/20/1996', 'Female'),
-(1, 'kupac', 'kupac', 'John', 'Haley', 'KUPAC', '12/20/1990', 'Male', true),
-(2, 'menadzer', 'menadzer', 'Blake', 'Evans', 'ADMIN', '12/30/1996', 'Female', true),
-(3, 'dostavljac', 'dostavljac', 'Bella', 'Miller', 'ADMIN', '12/20/1996', 'Female', true);
+(1, 'kupac', 'kupac', 'John', 'Haley', 'KUPAC', '12/20/1990', 'Male'),
+(2, 'menadzer', 'menadzer', 'Blake', 'Evans', 'ADMIN', '12/30/1996', 'Female'),
+(3, 'dostavljac', 'dostavljac', 'Bella', 'Miller', 'ADMIN', '12/20/1996', 'Female');
 
 INSERT INTO ARTIKAL(id, naziv, tipHrane, cijena, kolicina, opis, restoran) VALUES
 	(00, 'burger', Jelo, 500, 2, 'Burger sa 100% junecim mesom i cedar sirom', americki),
 	(11, 'pica', Jelo, 900, 2, 'Madjarica sa kulenom', italijanski),
-	(22, 'kroasan', Jelo, 230, 2, 'Kroasan sa marmeladom', francuski);
+	(22, 'kroasan', Jelo, 230, 2, 'Kroasan sa marmeladom', francuski),
+	(33, 'fanta', Pice, 100, 5, 'sok', francuski);
 
-INSERT INTO RESTORAN(id, naziv, tipHrane, cijena, kolicina, opis, restoran) VALUES
-	(00, 'burger', Jelo, 500, 2, 'Burger sa 100% junecim mesom i cedar sirom', americki),
+INSERT INTO RESTORAN(id, naziv, tipRestorana, artikli, lokacija, porudzbine) VALUES
+	(01, "Le Piaf", "francuski", "pasta", "Novi Sad", "porudzbina1"),
+	(02, "Pizzeria Ciao", "italijanski", "pica", "Novi Sad", "porudzbina2"),
 
-INSERT INTO MENADZER(id) SELECT ID FROM KORISNIK WHERE uloga = 'MENADZER';
+INSERT INTO MENADZER(id) SELECT ID FROM KORISNIK WHERE uloga = 'Menadzer';
 UPDATE MENADZER SET restoranid = 000;
 
 
